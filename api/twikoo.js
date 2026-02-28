c1e9210a063f0deb19708bc8044e3e42e53c62dd
MarkXu <xujianqiao86@gmail.com>
2026-02-28 19:58:03 +0800

    Refactor export logic for tkserver module
    Export app property from tkserver if available.

--- api/twikoo.js
+++ api/twikoo.js
@@ -1,2 +1,4 @@
-const { app } = require('tkserver');
-module.exports = app;
+// api/twikoo.js
+const tkserver = require('tkserver');
+// 如果 tkserver 有 app 屬性，則導出它，否則導出 tkserver 本身
+module.exports = tkserver.app || tkserver;
